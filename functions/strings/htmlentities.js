20fc9b20a36d25179c787173a58ee94a462e18cf
zehm <brandon@zehm.org>
2014-04-01 13:44:28 -0600

    Updated version of html_entities()
    Had to rewrite most of this function, because the original wouldn't properly quote:
        This is a "test"
    The parent version outputs:
        This is a &amp;quot;test&amp;quot;
    instead of:
        This is a &quot;test&quot;
    
    This version does remove a bit of functionality, but I think it reads better, and most importantly it works for me now :) Hopefully it helps someone else too.

--- functions/strings/htmlentities.js
+++ functions/strings/htmlentities.js
@@ -1,52 +1,48 @@
-function htmlentities(string, quote_style, charset, double_encode) {
-  //  discuss at: http://phpjs.org/functions/htmlentities/
-  // original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-  //  revised by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-  //  revised by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-  // improved by: nobbler
-  // improved by: Jack
-  // improved by: Rafał Kukawski (http://blog.kukawski.pl)
-  // improved by: Dj (http://phpjs.org/functions/htmlentities:425#comment_134018)
-  // bugfixed by: Onno Marsman
-  // bugfixed by: Brett Zamir (http://brett-zamir.me)
-  //    input by: Ratheous
-  //  depends on: get_html_translation_table
-  //   example 1: htmlentities('Kevin & van Zonneveld');
-  //   returns 1: 'Kevin &amp; van Zonneveld'
-  //   example 2: htmlentities("foo'bar","ENT_QUOTES");
-  //   returns 2: 'foo&#039;bar'
 
-  var hash_map = this.get_html_translation_table('HTML_ENTITIES', quote_style),
-    symbol = '';
-  string = string == null ? '' : string + '';
-
-  if (!hash_map) {
-    return false;
-  }
-
-  if (quote_style && quote_style === 'ENT_QUOTES') {
-    hash_map["'"] = '&#039;';
-  }
-
-  if ( !! double_encode || double_encode == null) {
+/////////////////////////////////////////////////////////////////////
+//  Function: htmlentities(string, quote_style=<ENT_QUOTES|ENT_COMPAT|ENT_NOQUOTES>)
+//  
+//  Description
+//      JS equivalent of PHP's htmlentities function.
+// 
+//  Notes:
+//    discuss at: http://phpjs.org/functions/htmlentities/
+//   original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+//    revised by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+//    revised by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+//   improved by: nobbler
+//   improved by: Jack
+//   improved by: Rafał Kukawski (http://blog.kukawski.pl)
+//   improved by: Dj (http://phpjs.org/functions/htmlentities:425#comment_134018)
+//   bugfixed by: Onno Marsman
+//   bugfixed by: Brett Zamir (http://brett-zamir.me)
+//      input by: Ratheous
+//    depends on: get_html_translation_table
+//     example 1: htmlentities('Kevin & van Zonneveld');
+//     returns 1: 'Kevin &amp; van Zonneveld'
+//     example 2: htmlentities("foo'bar","ENT_QUOTES");
+//     returns 2: 'foo&#039;bar'
+/////////////////////////////////////////////////////////////////////
+htmlentities = function(string, quote_style) {
+    var hash_map = {}, symbol = '', entity = '';
+    hash_map = get_html_translation_table('HTML_ENTITIES', quote_style);
+    if (hash_map === false) return false;
+    
+    // First loop replaces special characters with __NUMBER__
+    var symbols_used = {};
     for (symbol in hash_map) {
-      if (hash_map.hasOwnProperty(symbol)) {
-        string = string.split(symbol)
-          .join(hash_map[symbol]);
-      }
-    }
-  } else {
-    string = string.replace(/([\s\S]*?)(&(?:#\d+|#x[\da-f]+|[a-zA-Z][\da-z]*);|$)/g, function (ignore, text, entity) {
-      for (symbol in hash_map) {
         if (hash_map.hasOwnProperty(symbol)) {
-          text = text.split(symbol)
-            .join(hash_map[symbol]);
+            string = string.split(symbol).join('__'+symbol.charCodeAt(0)+'__');
+            symbols_used[symbol.charCodeAt(0)] = true;
         }
-      }
+    }
 
-      return text + entity;
-    });
-  }
-
-  return string;
-}
+    // Second loop replaces __NUMBER__ with the approperiate html entity
+    for (symbol in symbols_used) {
+        if (symbols_used.hasOwnProperty(symbol)) {
+            string = string.split('__'+symbol+'__').join(hash_map[String.fromCharCode(symbol)]);
+        }
+    }
+    
+    return string;
+};
